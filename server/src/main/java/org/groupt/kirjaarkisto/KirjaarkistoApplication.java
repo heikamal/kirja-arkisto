5a4e85151c0a0ab0b5fea632accc8debb38b1e64
heikamal <102971293+heikamal@users.noreply.github.com>
2024-01-24 14:00:41 +0200

    projektin alustus

---
+++ server/src/main/java/org/groupt/kirjaarkisto/KirjaarkistoApplication.java
@@ -0,0 +1,13 @@
+package org.groupt.kirjaarkisto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirjaarkistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirjaarkistoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/groupt/kirjaarkisto/KirjaarkistoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.groupt.kirjaarkisto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirjaarkistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
