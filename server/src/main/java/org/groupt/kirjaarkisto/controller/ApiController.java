e329beda3197c7bc52d05c5d0d9b74de3f90a4c8
heikamal <102971293+heikamal@users.noreply.github.com>
2024-02-08 11:12:35 +0200

    nopea testikontrolleri

---
+++ server/src/main/java/org/groupt/kirjaarkisto/controller/ApiController.java
@@ -0,0 +1,25 @@
+package org.groupt.kirjaarkisto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Testikontrolleri kehitystarkoituksiin. Pls ignore.
+ * 
+ * @author Heikki Malkavaara
+ *
+ */
+@RestController
+public class ApiController {
+
+    /**
+   * Käyttää @GetMapping -annotaatiota kartoittaakseen metodin GET-pyyntöön osoitteessa "/api". 
+   * Metodi palauttaa merkkijonon "Harri, voisitko hakee mulle tulisiemeniä?".
+   *
+   * @return         	Merkkijono "Harri, voisitko hakee mulle tulisiemeniä?"
+   */
+  @GetMapping("/api")
+  public String api() {
+    return "Harri, voisitko hakee mulle tulisiemeniä?";
+  }
+}
