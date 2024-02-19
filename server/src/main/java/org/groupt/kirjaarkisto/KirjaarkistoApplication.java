f53e9b15ceb6b06ae41b3d617c02352155abf199
heikamal <102971293+heikamal@users.noreply.github.com>
2024-02-19 15:39:02 +0200

    lisätty cors-configuraatio sallimaan kaikki /api/-osoitteeseen tulevat pyynnöt

--- server/src/main/java/org/groupt/kirjaarkisto/KirjaarkistoApplication.java
+++ server/src/main/java/org/groupt/kirjaarkisto/KirjaarkistoApplication.java
@@ -2,12 +2,26 @@ package org.groupt.kirjaarkisto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KirjaarkistoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KirjaarkistoApplication.class, args);
+	}
+
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/api/**");
+			}
+		};
 	}
 
 }
