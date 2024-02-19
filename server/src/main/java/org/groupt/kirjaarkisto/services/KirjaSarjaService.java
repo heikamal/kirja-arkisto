b459de96e0bda0a1c01f9560402b1b304772b287
heikamal <102971293+heikamal@users.noreply.github.com>
2024-02-19 14:08:16 +0200

    Lisätty operaatiot lisäämiselle ja poistamiselle

--- server/src/main/java/org/groupt/kirjaarkisto/services/KirjaSarjaService.java
+++ server/src/main/java/org/groupt/kirjaarkisto/services/KirjaSarjaService.java
@@ -1,5 +1,6 @@
 package org.groupt.kirjaarkisto.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.groupt.kirjaarkisto.models.KirjaSarja;
 import java.util.List;
@@ -9,15 +10,36 @@ import org.groupt.kirjaarkisto.repositories.KirjaSarjaRepository;
 public class KirjaSarjaService {
 
     @Autowired
-    private KirjaSarjaRepository KirjasarjaRepository;
+    private KirjaSarjaRepository kirjaSarjaRepository;
 
     public List<KirjaSarja> getKirjasarjat() {
-        return KirjasarjaRepository.findAll();
+        return kirjaSarjaRepository.findAll();
     }
 
     public KirjaSarja getKirjasarjaById(Long id) {
-        return KirjasarjaRepository.findById(id).orElse(null);
+        return kirjaSarjaRepository.findById(id).orElse(null);
     }
 
-    // Lisää tarvittavat liiketoimintalogiikkametodit
+    /**
+     * Tallentaa parametreina annetun kirjasarjan tietokantaan.
+     * 
+     * @param sarja tallennettava kirjasarja
+     * @return Tallennettu kirjasarja.
+     */
+    public KirjaSarja saveKirjaSarja(@NonNull KirjaSarja sarja) {
+      try {
+        return kirjaSarjaRepository.save(sarja);
+      } catch (IllegalArgumentException e) {
+        throw new IllegalArgumentException("Invalid input parameters!");
+      }
+    }
+
+    /**
+     * Poistaa kirjasarjan tietokannasta annetun ID-numeron perusteella. Ei palauta mitään.
+     * 
+     * @param id Poistettavan kirjasarjan ID kokonaislukuna.
+     */
+    public void removeKirjaSarja(@NonNull Long id) {
+        kirjaSarjaRepository.deleteById(id);
+    }
 }
