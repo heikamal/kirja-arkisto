f9d0ef39f9381eb368c88310e86d7aeace977ab7
heikamal <102971293+heikamal@users.noreply.github.com>
2024-02-20 16:35:14 +0200

    virheenkäsittely aloitettu

---
+++ server/src/main/java/org/groupt/kirjaarkisto/exceptions/NonExistingKirjaSarjaException.java
@@ -0,0 +1,19 @@
+package org.groupt.kirjaarkisto.exceptions;
+
+import org.groupt.kirjaarkisto.controller.errors.ErrorCode;
+
+/**
+ * Virheluokka tilanteelle jossa etsittyä kirjasarjaa ei ole saatavilla.
+ */
+public class NonExistingKirjaSarjaException extends RuntimeException implements ErrorCode {
+
+  public NonExistingKirjaSarjaException(final String message) {
+    super(message);
+  }
+
+  @Override
+  public String getErrorCode() {
+    return "NE-KS";
+  }
+
+}
