406fa690ee339ab480819fc3305043e26e02c65d
Kimboooo <102971438+Kimboooo@users.noreply.github.com>
2024-02-21 09:03:48 +0200

    kuvitus-todo

--- server/src/main/java/org/groupt/kirjaarkisto/models/Kuvitus.java
+++ server/src/main/java/org/groupt/kirjaarkisto/models/Kuvitus.java
@@ -3,19 +3,4 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "kuvitus")
 public class Kuvitus {
-    @EmbeddedId
-    private KuvitusId id;
-
-    @ManyToOne
-    @JoinColumn(name = "idkuva", insertable = false, updatable = false)
-    private Kuva kuva;
-
-    @ManyToOne
-    @JoinColumn(name = "idkirja", insertable = false, updatable = false)
-    private Kirja kirja;
-
-    @Column(name = "sivunro")
-    private Integer sivunro;
-
-    // getterit ja setterit
 }
