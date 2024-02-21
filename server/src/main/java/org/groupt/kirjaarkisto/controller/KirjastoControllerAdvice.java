06d09513bf9d663e8a4b30a96bffebbbedd5e826
heikamal <102971293+heikamal@users.noreply.github.com>
2024-02-21 18:03:18 +0200

    lisätty virheviesti jos kirjasarjaa ei löydy

--- server/src/main/java/org/groupt/kirjaarkisto/controller/KirjastoControllerAdvice.java
+++ server/src/main/java/org/groupt/kirjaarkisto/controller/KirjastoControllerAdvice.java
@@ -1,20 +1,40 @@
 package org.groupt.kirjaarkisto.controller;
 
+import org.groupt.kirjaarkisto.controller.errors.KirjaArkistoAppError;
 import org.groupt.kirjaarkisto.exceptions.NonExistingKirjaSarjaException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * Luokka, määrittämään sovelluksen virheenkäsittelymetodit.
  */
-
+@RestControllerAdvice
 public class KirjastoControllerAdvice {
+
+  //TODO: nämä enveiksi
+  //@Value("${kirjaarkisto.sendreport.uri}")
+  private String sendReportUri = "Huuda 'Heikki auta' niin kovaa kuin keuhkoista lähtee";
+  //@Value("${kirjaarkisto.api.version}")
+  private String currentApiVersion = "0.1";
 
   @ExceptionHandler(NonExistingKirjaSarjaException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public void handeNonExistingKirjaSarja() {
-    // testailen vielä
+  public ResponseEntity<KirjaArkistoAppError> handeNonExistingKirjaSarja() {
+    final KirjaArkistoAppError error = new KirjaArkistoAppError(
+      currentApiVersion,
+      Integer.toString(HttpStatus.NOT_FOUND.value()), 
+      "Kirjasarjaa ei löydy!",
+      "kirjasarja-exceptions", 
+      "Hakemaasi kirjasarjaa ei löydy tietokannasta.",
+      "Not found!",
+      sendReportUri
+    );
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
   }
   
 }
