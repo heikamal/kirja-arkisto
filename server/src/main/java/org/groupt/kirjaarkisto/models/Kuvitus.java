793409a2d2fe6ccdf81fb503d5faa1a747cbc5dd
Kimboooo <102971438+Kimboooo@users.noreply.github.com>
2024-02-22 22:04:03 +0200

    kuvitus-model luokka

--- server/src/main/java/org/groupt/kirjaarkisto/models/Kuvitus.java
+++ server/src/main/java/org/groupt/kirjaarkisto/models/Kuvitus.java
@@ -1,6 +1,22 @@
 package org.groupt.kirjaarkisto.models;
 import jakarta.persistence.*;
+
 @Entity
 @Table(name = "kuvitus")
 public class Kuvitus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idkuva;
+
+    @ManyToOne
+    @JoinColumn(name = "idkirja")
+    private Kirja kirja;
+
+    @ManyToOne
+    @JoinColumn(name = "idkuva")
+    private Kuva kuva;
+
+    private Integer sivunro;
+
 }
