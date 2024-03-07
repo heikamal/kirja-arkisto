a7c55d63a8486c21d9981b750d92dc227ff16764
heikamal <102971293+heikamal@users.noreply.github.com>
2024-03-07 12:01:53 +0200

    tehty kirjasarjan päivittämistä hieman selkeämmäksi

--- server/src/main/java/org/groupt/kirjaarkisto/controller/KirjaSarjaController.java
+++ server/src/main/java/org/groupt/kirjaarkisto/controller/KirjaSarjaController.java
@@ -60,24 +60,14 @@ public class KirjaSarjaController {
      * @return       ResponseEntity joka sisältää tietokantaan päivitetyn kirjasarjan tai virheen jos tiedoissa oli jotakin vialla.
      */
     @PutMapping(path = "/{id}")
-    public ResponseEntity<?> updateSarja(@PathVariable(value = "id") Long id, @RequestBody KirjaSarja sarja) {
-        KirjaSarja toBeModified = null;
-        KirjaSarja response = null;
-        // TODO: Parempi validointi
-        try{
-            toBeModified = kirjaSarjaService.getKirjasarjaById(id);
-        } catch (EntityNotFoundException e){
-            return new ResponseEntity<>("Invalid area ID!", HttpStatus.NOT_FOUND);
-        }
+    public KirjaSarja updateSarja(@PathVariable(value = "id") Long id, @RequestBody KirjaSarja sarja) {
+        KirjaSarja toBeModified = kirjaSarjaService.getKirjasarjaById(id);
+        
         toBeModified.setTitle(sarja.getTitle());
         toBeModified.setKustantaja(sarja.getKustantaja());
         toBeModified.setKuvaus(sarja.getKuvaus());
         toBeModified.setLuokittelu(sarja.getLuokittelu());
-        try {
-            response = kirjaSarjaService.saveKirjaSarja(toBeModified);
-        } catch (IllegalArgumentException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        
+        return kirjaSarjaService.saveKirjaSarja(toBeModified);
     }
 }
