a950e9d411c5a756fa3bab2d1ed8da6ba145260d
Kimboooo <102971438+Kimboooo@users.noreply.github.com>
2024-03-11 12:24:43 +0200

    get-setterit kuvaluokalle huom: autogeneroitu

--- server/src/main/java/org/groupt/kirjaarkisto/models/Kuva.java
+++ server/src/main/java/org/groupt/kirjaarkisto/models/Kuva.java
@@ -33,4 +33,96 @@ public class Kuva {
         
     }
 
+
+    /**
+     * @param idkuva the idkuva to set
+     */
+    public void setIdkuva(Long idkuva) {
+        this.idkuva = idkuva;
+    }
+
+    /**
+     * @return String return the kuvanimi
+     */
+    public String getKuvanimi() {
+        return kuvanimi;
+    }
+
+    /**
+     * @param kuvanimi the kuvanimi to set
+     */
+    public void setKuvanimi(String kuvanimi) {
+        this.kuvanimi = kuvanimi;
+    }
+
+    /**
+     * @return Integer return the julkaisuvuosi
+     */
+    public Integer getJulkaisuvuosi() {
+        return julkaisuvuosi;
+    }
+
+    /**
+     * @param julkaisuvuosi the julkaisuvuosi to set
+     */
+    public void setJulkaisuvuosi(Integer julkaisuvuosi) {
+        this.julkaisuvuosi = julkaisuvuosi;
+    }
+
+    /**
+     * @return String return the taiteilija
+     */
+    public String getTaiteilija() {
+        return taiteilija;
+    }
+
+    /**
+     * @param taiteilija the taiteilija to set
+     */
+    public void setTaiteilija(String taiteilija) {
+        this.taiteilija = taiteilija;
+    }
+
+    /**
+     * @return String return the tyyli
+     */
+    public String getTyyli() {
+        return tyyli;
+    }
+
+    /**
+     * @param tyyli the tyyli to set
+     */
+    public void setTyyli(String tyyli) {
+        this.tyyli = tyyli;
+    }
+
+    /**
+     * @return String return the kuvaus
+     */
+    public String getKuvaus() {
+        return kuvaus;
+    }
+
+    /**
+     * @param kuvaus the kuvaus to set
+     */
+    public void setKuvaus(String kuvaus) {
+        this.kuvaus = kuvaus;
+    }
+
+    /**
+     * @return String return the tiedostonimi
+     */
+    public String getTiedostonimi() {
+        return tiedostonimi;
+    }
+
+    /**
+     * @param tiedostonimi the tiedostonimi to set
+     */
+    public void setTiedostonimi(String tiedostonimi) {
+        this.tiedostonimi = tiedostonimi;
+    }
+
 }
