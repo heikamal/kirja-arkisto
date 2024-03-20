7b74c27f5288350571a6274920b4d41db85004c7
Kimboooo <102971438+Kimboooo@users.noreply.github.com>
2024-03-20 10:08:41 +0200

    kirjakopioille kuvat :3

---
+++ server/src/main/java/org/groupt/kirjaarkisto/models/Valokuva.java
@@ -0,0 +1,101 @@
+package org.groupt.kirjaarkisto.models;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "valokuva")
+public class Valokuva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idkuva")
+    private Long idkuva;
+
+    @Column(name = "kuvanimi")
+    private String kuvanimi;
+
+    @ManyToOne
+    @JoinColumn(name = "idkirjakopio")
+    private KirjaKopio kirjaKopio;
+
+    @Column(name = "sivunnro")
+    private Integer sivunnro;
+
+    @Column(name = "tiedostonimi")
+    private String tiedostonimi; 
+
+    
+    // Getters and setters
+
+    /**
+     * @return Long return the idkuva
+     */
+    public Long getIdkuva() {
+        return idkuva;
+    }
+
+    /**
+     * @param idkuva the idkuva to set
+     */
+    public void setIdkuva(Long idkuva) {
+        this.idkuva = idkuva;
+    }
+
+    /**
+     * @return String return the kuvanimi
+     */
+    public String getKuvanimi() {
+        return kuvanimi;
+    }
+
+    /**
+     * @param kuvanimi the kuvanimi to set
+     */
+    public void setKuvanimi(String kuvanimi) {
+        this.kuvanimi = kuvanimi;
+    }
+
+    /**
+     * @return KirjaKopio return the kirjaKopio
+     */
+    public KirjaKopio getKirjaKopio() {
+        return kirjaKopio;
+    }
+
+    /**
+     * @param kirjaKopio the kirjaKopio to set
+     */
+    public void setKirjaKopio(KirjaKopio kirjaKopio) {
+        this.kirjaKopio = kirjaKopio;
+    }
+
+    /**
+     * @return Integer return the sivunnro
+     */
+    public Integer getSivunnro() {
+        return sivunnro;
+    }
+
+    /**
+     * @param sivunnro the sivunnro to set
+     */
+    public void setSivunnro(Integer sivunnro) {
+        this.sivunnro = sivunnro;
+    }
+
+
+    /**
+     * @return String return the tiedostonimi
+     */
+    public String getTiedostonimi() {
+        return tiedostonimi;
+    }
+
+    /**
+     * @param tiedostonimi the tiedostonimi to set
+     */
+    public void setTiedostonimi(String tiedostonimi) {
+        this.tiedostonimi = tiedostonimi;
+    }
+
+}
+
