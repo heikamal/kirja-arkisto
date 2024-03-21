86c9cd03210d11071ed1674a97271baa1995d7ab
Potakka <102971224+Potakka@users.noreply.github.com>
2024-03-21 12:25:18 +0200

    kuvien lisäys alustavasti

--- app/src/app/photo/photo.component.ts
+++ app/src/app/photo/photo.component.ts
@@ -1,27 +1,88 @@
-import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpErrorResponse, HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-photo',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   providers: [DataService],
   templateUrl: './photo.component.html',
   styleUrl: './photo.component.css'
 })
 export class PhotoComponent {
-  constructor(private dataService: DataService, private httpClient: HttpClient) { }
-
-  selectedFile: File = new File([], 'default.jpg');
+  
+  photo_form: FormGroup;
+  selectedFile: any;
   retrievedImage: any;
   base64Data: any;
   retrieveResonse: any;
-  message: string = "";
+  message: any;
   imageName: any;
+
+  constructor(private dataService: DataService, fb: FormBuilder, private httpClient: HttpClient, private cookieService: CookieService) {
+    this.photo_form = fb.group({
+      tiedosto : [null],
+      julkaisuvuosi: [''],
+      taiteilija: ['', Validators.maxLength(45)],
+      tyyli: ['', Validators.maxLength(45)],
+      kuvaus:['', Validators.maxLength(255)],
+      sivunro: [''],
+      kuvannimi : ['', Validators.maxLength(45)]
+    });
+  }
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+    bookid : string = "1";
+
+    onUpload() {
+      if (this.photo_form.valid) {
+        const formData = new FormData();
+        Object.keys(this.photo_form.value).forEach(key => {
+          const value = this.photo_form.value[key];
+          formData.append(key, value);
+        });
+        const fileInput = document.querySelector('input[type="file"]') as HTMLInputElement;
+        if (fileInput) {
+          const files = fileInput.files;
+          if (files && files.length > 0) {
+            const file = files[0];
+            formData.append('tiedosto', file);
+          } else {
+            console.error('No files selected');
+          }
+        } else {
+          console.error('File input not found');
+        }
+        
+        const accessToken = this.cookieService.get('accessToken');
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${accessToken}`,
+        });
   
-}
+        this.httpClient.post("http://localhost:8080/api/kirjat/" + this.bookid + "/kuvat", formData, { headers, observe: 'response' })
+          .subscribe(
+            (response) => {
+              console.log(response);
+              if (response.status === 200) {
+                this.message = 'Image uploaded successfully';
+              } else {
+                this.message = 'Image not uploaded successfully';
+              }
+            },
+            (error: HttpErrorResponse) => {
+              console.error('Error:', error);
+              this.message = 'Error uploading image';
+            }
+          );
+      } else {
+        console.error('Form is invalid');
+      }
+    }
+  }
+  
