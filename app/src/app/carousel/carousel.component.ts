900099135a63dfabcfc2f2fbba9c30194501cc92
ComplexMonkeys <102974554+ComplexMonkeys@users.noreply.github.com>
2024-03-21 11:22:00 +0200

    sarjojen kirjat näkyville

--- app/src/app/carousel/carousel.component.ts
+++ app/src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Book } from '../book';
 import { CommonModule } from '@angular/common';
@@ -13,22 +13,45 @@ import { BookDetailsComponent } from '../book-details/book-details.component';
   templateUrl: './carousel.component.html'
 })
 export class CarouselComponent implements OnInit {
+  @Input() seriesId?: number;
   books: Book[] = [];
 
   constructor(private dataService: DataService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.dataService.get_books().subscribe((response: any[]) => {
-      this.books = response.map((bookData: any) => {
-        return {
-          id: bookData.id,
-          title: bookData.nimi,
-          author: bookData.kirjailija,
-          date: bookData.julkaisuVuosi,
-          series: bookData.jarjestysNro,
-          image_url: bookData.image_url
-        } as Book;
-      });
+      if (this.seriesId) {
+        this.books = response.filter(book => book.kirjaSarja.id === this.seriesId)
+          .map((bookData: any) => {
+            const book: Book = {
+              id: bookData.id,
+              title: bookData.nimi,
+              author: bookData.kirjailija,
+              date: bookData.julkaisuVuosi,
+              series: bookData.jarjestysNro,
+              image_url: bookData.image_url,
+              is_owned: false
+            };
+            this.dataService.get_ownership(book.id).subscribe((ownershipResponse: any) => {
+              const jsonStr: string = JSON.stringify(ownershipResponse);
+              const jsonObject: any = JSON.parse(jsonStr);
+              book.is_owned = jsonObject.owned;
+            });
+            return book;
+          });
+      } else {
+        this.books = response.map((bookData: any) => {
+          return {
+            id: bookData.id,
+            title: bookData.nimi,
+            author: bookData.kirjailija,
+            date: bookData.julkaisuVuosi,
+            series: bookData.jarjestysNro,
+            image_url: bookData.image_url,
+            is_owned: false
+          } as Book;
+        });
+      }
     });
   }
 
