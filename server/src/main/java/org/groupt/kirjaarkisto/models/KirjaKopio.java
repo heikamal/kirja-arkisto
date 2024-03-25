84c71c8dce3d6c8c630caaaeba695ea9ef3d2762
heikamal <102971293+heikamal@users.noreply.github.com>
2024-03-25 10:38:12 +0200

    valokuva korjattu

--- server/src/main/java/org/groupt/kirjaarkisto/models/KirjaKopio.java
+++ server/src/main/java/org/groupt/kirjaarkisto/models/KirjaKopio.java
@@ -1,252 +1,258 @@
 package org.groupt.kirjaarkisto.models;
+
 import java.sql.Date;
+import java.util.List;
+
 import org.groupt.kirjaarkisto.payload.KirjaKopioDTO;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "kirjakopio")
 public class KirjaKopio {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idkirjakopio")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "idkirjakopio")
+  private Long id;
 
-    @Column(name = "kirjanimi")
-    private String title;
+  @Column(name = "kirjanimi")
+  private String title;
 
-    @Column(name = "painos")
-    private Integer editions;
+  @Column(name = "painos")
+  private Integer editions;
 
-    @Column(name = "painosvuosi")
-    private Integer editionYear;
+  @Column(name = "painosvuosi")
+  private Integer editionYear;
 
-    @ManyToOne
-    @JoinColumn(name = "idkirja")
-    private Kirja book;
+  @ManyToOne
+  @JoinColumn(name = "idkirja")
+  private Kirja book;
 
-    @Column(name = "hankintahinta")
-    private Double purchasePrice;
+  @Column(name = "hankintahinta")
+  private Double purchasePrice;
 
-    @Column(name = "hankintapvm")
-    private Date purchaseDate;
+  @Column(name = "hankintapvm")
+  private Date purchaseDate;
 
-    @Column(name = "kunto")
-    private Integer condition;
+  @Column(name = "kunto")
+  private Integer condition;
 
-    @Column(name = "kuvaus")
-    private String description;
+  @Column(name = "kuvaus")
+  private String description;
 
-    @Column(name = "myyntipvm")
-    private Date saleDate;
+  @Column(name = "myyntipvm")
+  private Date saleDate;
 
-    @Column(name = "myyntihinta")
-    private Double salePrice;
+  @Column(name = "myyntihinta")
+  private Double salePrice;
 
-    @Column(name = "idkirjahylly")
-    private Long idKirjaHylly;
+  @Column(name = "idkirjahylly")
+  private Long idKirjaHylly;
 
-    @Column(name = "idkirjasarja")
-    private Long idKirjaSarja;
+  @Column(name = "idkirjasarja")
+  private Long idKirjaSarja;
 
-    public KirjaKopio() {
-        
-    }
+  public KirjaKopio() {
 
-    public KirjaKopio(KirjaKopioDTO dto, Kirja kirja, Long idKirjaHylly) {
-      this.title = dto.getNimi();
-      this.editions = dto.getPainos();
-      this.editionYear = dto.getPainosVuosi();
-      this.book = kirja;
-      this.purchasePrice = dto.getOstoHinta();
-      this.purchaseDate = dto.getOstoPvm();
-      this.condition = dto.getKunto();
-      this.description = dto.getKuvaus();
-      this.saleDate = dto.getMyyntiPvm();
-      this.salePrice = dto.getMyyntiHinta();
-      this.idKirjaHylly = idKirjaHylly;
-      this.idKirjaSarja = dto.getIdKirjaSarja();
-    }
+  }
 
-    /**
-     * @return Long return the id
-     */
-    public Long getId() {
-        return id;
-    }
+  public KirjaKopio(KirjaKopioDTO dto, Kirja kirja, Long idKirjaHylly) {
+    this.title = dto.getNimi();
+    this.editions = dto.getPainos();
+    this.editionYear = dto.getPainosVuosi();
+    this.book = kirja;
+    this.purchasePrice = dto.getOstoHinta();
+    this.purchaseDate = dto.getOstoPvm();
+    this.condition = dto.getKunto();
+    this.description = dto.getKuvaus();
+    this.saleDate = dto.getMyyntiPvm();
+    this.salePrice = dto.getMyyntiHinta();
+    this.idKirjaHylly = idKirjaHylly;
+    this.idKirjaSarja = dto.getIdKirjaSarja();
+  }
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+  /**
+   * @return Long return the id
+   */
+  public Long getId() {
+    return id;
+  }
 
-    /**
-     * @return String return the title
-     */
-    public String getTitle() {
-        return title;
-    }
+  /**
+   * @param id the id to set
+   */
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    /**
-     * @param title the title to set
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  /**
+   * @return String return the title
+   */
+  public String getTitle() {
+    return title;
+  }
 
-    /**
-     * @return Integer return the editions
-     */
-    public Integer getEditions() {
-        return editions;
-    }
+  /**
+   * @param title the title to set
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    /**
-     * @param editions the editions to set
-     */
-    public void setEditions(Integer editions) {
-        this.editions = editions;
-    }
+  /**
+   * @return Integer return the editions
+   */
+  public Integer getEditions() {
+    return editions;
+  }
 
-    /**
-     * @return Integer return the editionYear
-     */
-    public Integer getEditionYear() {
-        return editionYear;
-    }
+  /**
+   * @param editions the editions to set
+   */
+  public void setEditions(Integer editions) {
+    this.editions = editions;
+  }
 
-    /**
-     * @param editionYear the editionYear to set
-     */
-    public void setEditionYear(Integer editionYear) {
-        this.editionYear = editionYear;
-    }
+  /**
+   * @return Integer return the editionYear
+   */
+  public Integer getEditionYear() {
+    return editionYear;
+  }
 
-    /**
-     * @return kirja return the book
-     */
-    public Kirja getBook() {
-        return book;
-    }
+  /**
+   * @param editionYear the editionYear to set
+   */
+  public void setEditionYear(Integer editionYear) {
+    this.editionYear = editionYear;
+  }
 
-    /**
-     * @param book the book to set
-     */
-    public void setBook(Kirja book) {
-        this.book = book;
-    }
+  /**
+   * @return kirja return the book
+   */
+  public Kirja getBook() {
+    return book;
+  }
 
-    /**
-     * @return Double return the purchasePrice
-     */
-    public Double getPurchasePrice() {
-        return purchasePrice;
-    }
+  /**
+   * @param book the book to set
+   */
+  public void setBook(Kirja book) {
+    this.book = book;
+  }
 
-    /**
-     * @param purchasePrice the purchasePrice to set
-     */
-    public void setPurchasePrice(Double purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
+  /**
+   * @return Double return the purchasePrice
+   */
+  public Double getPurchasePrice() {
+    return purchasePrice;
+  }
 
-    /**
-     * @return Date return the purchaseDate
-     */
-    public Date getPurchaseDate() {
-        return purchaseDate;
-    }
+  /**
+   * @param purchasePrice the purchasePrice to set
+   */
+  public void setPurchasePrice(Double purchasePrice) {
+    this.purchasePrice = purchasePrice;
+  }
 
-    /**
-     * @param purchaseDate the purchaseDate to set
-     */
-    public void setPurchaseDate(Date purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
+  /**
+   * @return Date return the purchaseDate
+   */
+  public Date getPurchaseDate() {
+    return purchaseDate;
+  }
 
-    /**
-     * @return Integer return the condition
-     */
-    public Integer getCondition() {
-        return condition;
-    }
+  /**
+   * @param purchaseDate the purchaseDate to set
+   */
+  public void setPurchaseDate(Date purchaseDate) {
+    this.purchaseDate = purchaseDate;
+  }
 
-    /**
-     * @param condition the condition to set
-     */
-    public void setCondition(Integer condition) {
-        this.condition = condition;
-    }
+  /**
+   * @return Integer return the condition
+   */
+  public Integer getCondition() {
+    return condition;
+  }
 
-    /**
-     * @return String return the description
-     */
-    public String getDescription() {
-        return description;
-    }
+  /**
+   * @param condition the condition to set
+   */
+  public void setCondition(Integer condition) {
+    this.condition = condition;
+  }
 
-    /**
-     * @param description the description to set
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  /**
+   * @return String return the description
+   */
+  public String getDescription() {
+    return description;
+  }
 
-    /**
-     * @return Date return the saleDate
-     */
-    public Date getSaleDate() {
-        return saleDate;
-    }
+  /**
+   * @param description the description to set
+   */
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    /**
-     * @param saleDate the saleDate to set
-     */
-    public void setSaleDate(Date saleDate) {
-        this.saleDate = saleDate;
-    }
+  /**
+   * @return Date return the saleDate
+   */
+  public Date getSaleDate() {
+    return saleDate;
+  }
 
-    /**
-     * @return Double return the salePrice
-     */
-    public Double getSalePrice() {
-        return salePrice;
-    }
+  /**
+   * @param saleDate the saleDate to set
+   */
+  public void setSaleDate(Date saleDate) {
+    this.saleDate = saleDate;
+  }
 
-    /**
-     * @param salePrice the salePrice to set
-     */
-    public void setSalePrice(Double salePrice) {
-        this.salePrice = salePrice;
-    }
+  /**
+   * @return Double return the salePrice
+   */
+  public Double getSalePrice() {
+    return salePrice;
+  }
 
-    /**
-     * @return Long return the idKirjaHylly
-     */
-    public Long getIdKirjaHylly() {
-        return idKirjaHylly;
-    }
+  /**
+   * @param salePrice the salePrice to set
+   */
+  public void setSalePrice(Double salePrice) {
+    this.salePrice = salePrice;
+  }
 
-    /**
-     * @param idKirjaHylly the idKirjaHylly to set
-     */
-    public void setIdKirjaHylly(Long idKirjaHylly) {
-        this.idKirjaHylly = idKirjaHylly;
-    }
+  /**
+   * @return Long return the idKirjaHylly
+   */
+  public Long getIdKirjaHylly() {
+    return idKirjaHylly;
+  }
 
-    /**
-     * @return Long return the idKirjaSarja
-     */
-    public Long getIdKirjaSarja() {
-        return idKirjaSarja;
-    }
+  /**
+   * @param idKirjaHylly the idKirjaHylly to set
+   */
+  public void setIdKirjaHylly(Long idKirjaHylly) {
+    this.idKirjaHylly = idKirjaHylly;
+  }
 
-    /**
-     * @param idKirjaSarja the idKirjaSarja to set
-     */
-    public void setIdKirjaSarja(Long idKirjaSarja) {
-        this.idKirjaSarja = idKirjaSarja;
-    }
+  /**
+   * @return Long return the idKirjaSarja
+   */
+  public Long getIdKirjaSarja() {
+    return idKirjaSarja;
+  }
+
+  /**
+   * @param idKirjaSarja the idKirjaSarja to set
+   */
+  public void setIdKirjaSarja(Long idKirjaSarja) {
+    this.idKirjaSarja = idKirjaSarja;
+  }
 }
